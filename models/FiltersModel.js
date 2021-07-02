1137f5c8738433b05dc2a21bc83769d95ac1a85e
“uipowerranger” <uipowerranger@gmail.com>
2021-07-02 11:51:00 +0530

    adding iseditable field to Home Filters

--- models/FiltersModel.js
+++ models/FiltersModel.js
@@ -1,4 +1,4 @@
-var mongoose = require("mongoose");
+var mongoose = require( "mongoose" );
 var Schema = mongoose.Schema;
 
 var FiltersSchema = new mongoose.Schema(
@@ -6,8 +6,10 @@ var FiltersSchema = new mongoose.Schema(
     user: { type: Schema.ObjectId, ref: "users", required: false },
     status: { type: Number, required: true, default: 1 },
     filter_name: { type: String, required: true },
+
+    iseditable: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("filters", FiltersSchema);
+module.exports = mongoose.model( "filters", FiltersSchema );
